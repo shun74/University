b90ba62e847087e3b9ed53a3afb34d357b1ad14f
shun74 <syunnsyunn74@gmail.com>
2021-05-28 13:47:45 +0900

    Straight List2 & Biodirectional List completed

--- DataStructure/BidirectionalList.c
+++ DataStructure/BidirectionalList.c
@@ -4,31 +4,28 @@ typedef int Element;
 
 typedef struct node_tag{
     Element value;
-    struct node_tag *next;
+    struct node_tag *left, *right;
 } Node;
 
 typedef Node *NodePointer;
 
-typedef struct{
+typedef struct List{
     NodePointer head;
     NodePointer current;
 } List;
 
 void Create(List *L){
-    NodePointer p;
-    p = malloc(sizeof(Node));
-    p->next = NULL;
-    L->head = p;
-    L->current = NULL;
+    L->head = NULL;
+    L-> current = NULL;
 }
 
 int Size(List *L){
     NodePointer p;
     p = malloc(sizeof(Node));
-    p = L->head->next;
+    p = L->head;
     int count=0;
     while(p!=NULL){
-        p = p->next;
+        p = p->right;
         count++;
     }
     return count;
@@ -37,7 +34,7 @@ int Size(List *L){
 int CurPos(List *L){
     NodePointer p;
     p = malloc(sizeof(Node));
-    p = L->current->next;
+    p = L->current;
     if(p->value || p->value==0){
         return p->value;
     } else {
@@ -46,7 +43,7 @@ int CurPos(List *L){
 }
 
 int Findith(List *L, int i){
-    if(L->head->next==NULL){
+    if(L->head==NULL){
         return 0;
     }
     NodePointer p;
@@ -54,31 +51,26 @@ int Findith(List *L, int i){
     p = L->head;
     for(int j=0; j<i; j++){
         if(p==NULL) return 0;
-        p = p->next;
+        p = p->right;
     }
     L->current = p;
     return 1;
 }
 
 int FindRight(List *L){
-    if(L->head->next==NULL || L->current==NULL) return 0;
+    if(L->current == NULL) return 0;
+    else if(L->current->right == NULL) return 0;
     else {
-        L->current = L->current->next;
+        L->current = L->current->right;
         return 1;
     }
 }
 
 int FindLeft(List *L){
-    NodePointer p, q;
     if(L->current == NULL) return 0;
-    else if(L->current == L->head) return 0;
+    else if(L->current->left == NULL) return 0;
     else {
-        p = L->current;
-        q = L->head;
-        while (q->next != p){
-            q = q->next;
-        }
-        L->current = q;
+        L->current = L->current->left;
         return 1;
     }
 }
@@ -86,118 +78,125 @@ int FindLeft(List *L){
 int InsertRight(List *L, Element e){
     NodePointer p;
     p = malloc(sizeof(Node));
-    if(L->head->next == NULL){
+    if(L->head == NULL){
         p->value = e;
-        p->next = NULL;
-        L->head->next = p;
-        L->current = L->head->next;
-        return 1;
-    } else if(L->current->next==NULL){
-        p->value = e;
-        p->next = NULL;
-        L->current->next = p;
+        p->right = NULL;
+        p->left = NULL;
+        L->head = p;
+        L->current = p;
         return 1;
     } else {
         p->value = e;
-        p->next = L->current->next->next;
-        L->current->next->next = p;
+        if(L->current->right==NULL){
+            p->right = NULL;
+        } else {
+            p->right = L->current->right;
+            L->current->right->left = p;
+        }
+        p->left = L->current;
+        L->current->right = p;
+        L->current = p;
         return 1;
     }
 }
 
 int InsertLeft(List *L, Element e){
     NodePointer p;
-    if (L->current == NULL){
-        if(L->head->next != NULL) return 0;
-        else {
-            p = malloc(sizeof(Node));
-            p->value = e;
-            p->next = NULL;
-            L->head->next = p;
-            L->current = L->head->next;
-            return 1;
-        }
-    } else {
-        p = malloc(sizeof(Node));
+    p = malloc(sizeof(Node));
+    if(L->head == NULL){
         p->value = e;
-        p->next = L->current->next;
-        L->current->next = p;
+        p->right = NULL;
+        p->left = NULL;
+        L->head = p;
+        L->current = p;
+        return 1;
+    } else {
+        p->value = e;
+        p->left = L->current->left;
+        p->right = L->current;
+        L->current->left->right = p;
+        L->current->left = p;
+        L->current = p;
         return 1;
     }
 }
 
 int Delete(List *L){
-    NodePointer p;
+    NodePointer p, q, r;
+    p = malloc(sizeof(Node));
+    q = malloc(sizeof(Node));
+    r = malloc(sizeof(Node));
     if(L->current == NULL) return 0;
-    else {
-        p = L->current->next;
-        L->current->next = p->next;
-        free(p);
+    else if(L->head->right == NULL){
+        p = L->current;
+        L->head = L->current = NULL;
+    } else {
+        if(L->current->left == NULL){
+            L->current->right->left = NULL;
+            L->head = L->current = L->current->right;
+        } else if(L->current->right == NULL){
+            L->current->left->right = NULL;
+            L->current = L->current->left;
+        } else {
+            L->current->left->right = L->current->right;
+            L->current->right->left = L->current->left;
+            L->current = L->current->right;
+        }
     }
-    if(L->current->next ==NULL) {
-        L->current = NULL;
-    }
+    free(p);
     return 1;
 }
 
 int Retrieve(List *L, Element *v){
-    if(L->current==NULL || L->current->next==NULL) return 0;
+    if(L->current == NULL) return 0;
     else {
-        *v = L->current->next->value;
+        *v = L->current->value;
         return 1;
     }
 }
 
-int Update(List *L, Element e){
-    if(L->current==NULL || L->current->next==NULL) return 0;
-    else {
-        L->current->next->value = e;
-        return 1;
-    }
-}
-
-void Copy(List *L, List *L2 ){
+void Copy( List *L, List *L2 ){
     NodePointer p,q;
     p = malloc(sizeof(Node));
     q = malloc(sizeof(Node));
-    p = L->head->next;
+    p = L->head;
     while(p!=NULL){
         InsertRight(L2, p->value);
         if(L->current==p){
             q = L2->current;
         }
-        p = p->next;
+        p = p->right;
     }
     L2->current = q;
 }
 
-int Equal( List *L, List *L2 ){
+int Equal(List *L, List *L2 ){
     if(Size(L)!=Size(L2)) return 0;
     NodePointer p,q;
     p = malloc(sizeof(Node));
     q = malloc(sizeof(Node));
-    p = L->head->next;
-    q = L2->head->next;
+    p = L->head;
+    q = L2->head;
     while(p!=NULL){
         if(p->value!=q->value) return 0;
-        p = p->next;
-        q = q->next;
+        p = p->right;
+        q = q->right;
     }
     return 1;
 }
 
 void printList(List *L){
-    if(L->head == NULL || L->current==NULL) {
+    if(L->head==NULL|| L->current==NULL) {
         printf("Empty List\n");
         return;
     }
     NodePointer p;
     p = malloc(sizeof(Node));
-    p = L->head->next;
+    p = L->head;
     printf("Current List: ");
     while(p!=NULL){
         printf("%d ",p->value);
-        p = p->next;
+        p = p->right;
     }
     printf("\n");
 }
@@ -205,9 +204,9 @@ void printList(List *L){
 int main(){
     // Check Functions.
     List lst;
+    Element a=0;
     Create(&lst);
     // Result of each function -> "Function name + ? + 0or1".
-    Element a=0;
     printf("Insert? %d\n",InsertRight(&lst, a));
     a=10;
     printf("Insert? %d\n",InsertRight(&lst, a));
@@ -226,8 +225,10 @@ int main(){
     printList(&lst);
 
     printf("FindRight? %d\n",FindRight(&lst));
+    printf("Current value = %d\n", CurPos(&lst));
     printf("Deleted? %d\n", Delete(&lst));
     // &lst: 0->30->20
+    printf("Current value = %d\n", CurPos(&lst));
     printList(&lst);
 
     Element *b;
